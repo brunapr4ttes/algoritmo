b86530c7233f3444587787c2627e2f098e961ab3
brunapr4ttes <brunapratesrobotica@gmail.com>
2023-07-25 11:08:14 -0300

    Atividade 1, Vídeo Locadora, realizada

--- 02_ESTRUTURA_SEQUENCIAL/atividades/atividade1.js
+++ 02_ESTRUTURA_SEQUENCIAL/atividades/atividade1.js
@@ -1,1 +1,15 @@
-alert("Arquivo atividade")
+alert("Arquivo atividade 1")
+
+let título, duração, horas, minutos
+
+título = prompt("Qual será o filme? ")
+duração = prompt("Qual a duração do filme em minutos? ")
+
+horas = Math.floor(duração / 60)
+minutos = duração % 60
+
+duração = Number(duração)
+horas = Number(horas)
+minutos = Number(minutos)
+
+document.write(`O filme ${título} tem duração de ${horas} horas e ${minutos} minutos`)
