5213078bdc7a67f82626d2ac3f582eb7709b41cf
brunapr4ttes <brunapratesrobotica@gmail.com>
2023-07-27 08:04:46 -0300

    03_tipo_texto.html atualizado com informações adicionais 2

--- 02_ESTRUTURA_SEQUENCIAL/atividades/atividade6.js
+++ 02_ESTRUTURA_SEQUENCIAL/atividades/atividade6.js
@@ -6,4 +6,4 @@ preco = prompt(`Preço padrão do produto: `)
 promocao = preco * 0.5
 tresprodutos = (preco * 2) + promocao
   
-    document.write(`50% de desconto na compra de três unidades de ${produto}! Preço padrão por unidade: R$${preco}. Preço com 50% de desconto (para um item) na compra de três unidades: R$${tresprodutos}.`);
+    document.write(`50% de desconto na compra de três unidades de ${produto}! <br> Preço padrão por unidade: R$${preco}. <br>Preço com 50% de desconto (para um item) na compra de três unidades: R$${tresprodutos}.`);
