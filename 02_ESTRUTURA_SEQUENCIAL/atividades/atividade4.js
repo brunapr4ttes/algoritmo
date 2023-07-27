19ca09cea2c5de22dec9ec64b2223717582738cb
bruna <brunapratesrobotica@gmail.com>
2023-07-26 22:28:17 -0300

    Atividade 4, Promoção de centavos de um medicamento, realizada

--- 02_ESTRUTURA_SEQUENCIAL/atividades/atividade4.js
+++ 02_ESTRUTURA_SEQUENCIAL/atividades/atividade4.js
@@ -1,23 +1,12 @@
-function calculatePromotionalPrice(description, price) {
-    // Calculate the total price for two units of the medication
-    const total = price * 2;
-  
-    // Get the centavos (fractional part) of the total price
-    const fractionalPart = total % 1;
-  
-    // Calculate the discount by rounding down the fractional part to the nearest centavo
-    const discount = Math.floor(fractionalPart * 100) / 100;
-  
-    // Calculate the promotional price by subtracting the discount from the total price
-    const promotionalPrice = total - discount;
-  
-    return promotionalPrice;
-  }
-  
-  // Test the function with sample input
-  const description = prompt("Enter the medication description:");
-  const price = parseFloat(prompt("Enter the price of one unit of the medication:"));
-  
-  const promotionalPrice = calculatePromotionalPrice(description, price);
-  console.log(`Promotional Price for ${description}: R$${promotionalPrice.toFixed(2)}`);
+let promocao, preco, cents, total, medicamento, valorFinal
+
+medicamento = prompt("Qual o medicamento? ")
+preco = prompt("Qual o preço do medicamento? ")
+
+  total = preco * 2;
+  cents = total % 1;
+  promocao = Math.floor(cents * 100) / 100;
+  valorFinal = total - cents;
+
+  document.write(`A promoção para duas unidades de ${medicamento} é de R$${promocao.toFixed(2)} e o valor final é de R$${valorFinal}`);
   
