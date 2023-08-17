2fcb29b70e2341c3308dfe350d5e8f7215e178dc
brunapratesrobotica@gmail.com <brunapratesrobotica>
2023-08-17 12:41:03 -0300

    Arquivo scriptnumtabuada.js atualizado e finalizado 2

--- 04_ESTRUTURA_REPETICAO/scriptnumtabuada.js
+++ 04_ESTRUTURA_REPETICAO/scriptnumtabuada.js
@@ -4,5 +4,5 @@ numero = Number(prompt(`Digite um número e veja a tabuada: `))
 
 for(let tab = 1; tab <= 10; tab++){
  let resultado = numero * tab 
- document.write(numero+" x "+tab+" = "+resultado) 
+ document.write(numero+" x "+tab+" = "+resultado+"<br>") 
 }
